3fe35d4af5602aff67f3432b5a2b7ba8aeed47fa
yohang <yohan@giarel.li>
2015-12-28 19:52:44 +0100

    Made updates for cordova application

--- src/jsx/app.jsx
+++ src/jsx/app.jsx
@@ -1,23 +1,39 @@
 import React from 'react';
-import { render } from 'react-dom'
-import createBrowserHistory from 'history/lib/createBrowserHistory'
+import { render } from 'react-dom';
+import createBrowserHistory from 'history/lib/createBrowserHistory';
+import createHashHistory from 'history/lib/createHashHistory';
 import { Router, Route, Link, Redirect } from 'react-router';
 import Config from './constants/Config';
 import WatchLinks from './components/WatchLinks';
 
 let $element = document.getElementById('app');
+let createHistory = window.location.protocol === 'file:' ? createHashHistory : createBrowserHistory;
 
 Config.ENTRYPOINT = $element.getAttribute('data-entrypoint');
 
-render(
-    (
-        <Router history={createBrowserHistory()}>
-            <Redirect from="/" to="/page/1" />
-            <Redirect from="/tags//" to="/page/1" />
-            <Redirect from="/tags/:tags" to="/tags/:tags/page/1" />
-            <Route path="/page/:page" component={WatchLinks} />
-            <Route path="/tags/:tags/page/:page" component={WatchLinks} />
-        </Router>
-    ),
-    $element
-);
+class App extends React.Component
+{
+    render() {
+        return (
+            <Router history={createHistory()}>
+                <Redirect from="/" to="/page/1" />
+                <Redirect from="/tags//" to="/page/1" />
+                <Redirect from="/tags/:tags" to="/tags/:tags/page/1" />
+                <Route path="/page/:page" component={WatchLinks} />
+                <Route path="/tags/:tags/page/:page" component={WatchLinks} />
+            </Router>
+        );
+    }
+}
+
+function boot() {
+    render(<App />, $element);
+}
+
+if (window.location.protocol === 'file:') {
+    document.addEventListener('deviceready', boot, false);
+} else {
+    boot();
+}
+
+
