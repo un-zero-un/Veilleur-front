a6244b62fc7bc1869d1b202c22ce35046c9dc300
yohang <yohan@giarel.li>
2015-12-28 17:52:26 +0100

    Made some style. Yeah, I know, i shouldn't. But I did.

--- src/jsx/components/Tags.jsx
+++ src/jsx/components/Tags.jsx
@@ -12,8 +12,9 @@ function getInitialState(props) {
     }
 
     return {
-        tags:       TagStore.getAll(),
-        activeTags: tags
+        tags:        TagStore.getAll(),
+        activeTags:  tags,
+        tagListOpen: false
     };
 }
 
@@ -53,10 +54,23 @@ export default class extends React.Component {
         return -1 !== this.state.activeTags.indexOf(tag.name);
     }
 
+    toggleTagList() {
+        this.state.tagListOpen = !this.state.tagListOpen;
+
+        this.setState(this.state);
+    }
+
     render() {
         return (
             <nav className="tags-nav">
-                <ul className="tags-list">
+                <a className="tag-list-opener" onClick={this.toggleTagList.bind(this)}>
+                    <svg viewBox="0 0 30 40" className={'tags-nav-icon ' + (this.state.tagListOpen ? 'active' : '')}>
+                        <line x1="2" y1="5" x2="38" y2="5" className="top-line" />
+                        <line x1="2" y1="20" x2="38" y2="20" className="middle-line" />
+                        <line x1="2" y1="35" x2="38" y2="35" className="bottom-line" />
+                    </svg>
+                </a>
+                <ul className={'tags-list ' + (this.state.tagListOpen ? 'open' : 'closed')}>
                     {this.state.tags.map((tag) => {
                         return (
                             <li key={tag.id}
